2b0ec203cbddc88c7f3b6723f5e95a3f6303da78
murph77 <91634541+murph77@users.noreply.github.com>
2022-05-01 21:05:16 -0700

    Update skillDemoTest.java

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -5,6 +5,6 @@ import org.junit.*;
 public class skillDemoTest {
     @Test
     public void multiply(){
-        assertEquals(0, skillDemo.multiply(1,0)); //meant to fail
+        assertEquals(1, skillDemo.multiply(1,0)); //meant to fail
     }
 }
